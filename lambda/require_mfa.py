48a8e738f2f7dad0c2d2554ce4fa0acd916c9b1d
Kevin Bowers <kevin.bowers@kudelskisecurity.com>
2019-01-09 14:37:12 -0500

    Python 3.6 compatibility; re-named mfa script; updated gitignore

--- lambda/require_mfa.py
+++ lambda/require_mfa.py
@@ -1,46 +1,58 @@
-from __future__ import print_function
-
-import json
 import boto3
-from botocore.exceptions import ClientError
-import sys
+import json
 import os
+import sys
+from botocore.exceptions import ClientError
+
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, sort_keys=True))
 
     # Error Checking goes first
-    if event['source'] != "aws.iam" : sys.exit(0)
-
+    # if event['source'] != "aws.iam":
+    #     sys.exit(0)
     api_call = event['detail']['eventName']
-    if api_call == "CreateLoginProfile" :
-        process_CreateLoginProfile(event,context)
+    if api_call == "CreateLoginProfile":
+        process_CreateLoginProfile(event, context)
         return 0
-    elif api_call == "EnableMFADevice" :
-        process_EnableMFADevice(event,context)
+    elif api_call == "EnableMFADevice":
+        process_EnableMFADevice(event, context)
         return 0
-    elif api_call == "DeactivateMFADevice" :
-        process_DeactivateMFADevice(event,context)
+    elif api_call == "DeactivateMFADevice":
+        process_DeactivateMFADevice(event, context)
         return 0
     else:
+        print("Invalid API Call: " + api_call)
         raise Exception("Invalid API Call: " + api_call)
+
 
 def process_CreateLoginProfile(event, context):
     # Get the important bits
     username = event['detail']['responseElements']['loginProfile']['userName']
-    
+
     # Verify there is no MFA present
     client = boto3.client('iam')
-    response = client.list_mfa_devices( UserName=username )
+    response = client.list_mfa_devices(UserName=username)
 
-    if len(response['MFADevices']) == 0 :
+    if len(response['MFADevices']) == 0:
         # There is no MFA enabled
-        print (username + " does not have MFA. Adding to blackhole Group")
+        print(username + " does not have MFA. Adding to blackhole Group")
         add_user_to_blackhole(username)
-    else :
-        print (username + " has an MFA. Removing from blackhole Group")
+    else:
+        print(username + " has an MFA. Removing from blackhole Group")
         remove_user_from_blackhole(username)
     print("CreateLoginProfile Execution Complete")
+
+
+def process_DeleteLoginProfile(event, context):
+    # Get the important bits
+    username = event['detail']['responseElements']['loginProfile']['userName']
+
+    # No login profile, so MFA is not required
+    client = boto3.client('iam')
+    remove_user_from_blackhole(username)
+    print("DeleteLoginProfile Execution Complete")
+
 
 def process_EnableMFADevice(event, context):
     # Get the important bits
@@ -48,31 +60,32 @@ def process_EnableMFADevice(event, context):
 
     # Verify there is an MFA present
     client = boto3.client('iam')
-    response = client.list_mfa_devices( UserName=username )
-    if len(response['MFADevices']) >= 1 :
+    response = client.list_mfa_devices(UserName=username)
+    if len(response['MFADevices']) >= 1:
         # There is now an MFA
-        print (username + " has activated their MFA. Removing from blackhole Group")
+        print(username + " has activated their MFA. Removing from blackhole Group")
         remove_user_from_blackhole(username)
-    else :
-        print (username + " has no MFA. Adding to blackhole Group")
+    else:
+        print(username + " has no MFA. Adding to blackhole Group")
         add_user_to_blackhole(username)
     print("EnableMFADevice Execution Complete")
+
 
 def process_DeactivateMFADevice(event, context):
     # Get the important bits
     username = event['detail']['requestParameters']['userName']
-    
+
     # Verify there is no MFA present
     client = boto3.client('iam')
     try:
-        response = client.list_mfa_devices( UserName=username )
+        response = client.list_mfa_devices(UserName=username)
 
-        if len(response['MFADevices']) == 0 :
+        if len(response['MFADevices']) == 0:
             # There is no MFA enabled
-            print (username + " does not have MFA. Adding to blackhole Group")
+            print(username + " does not have MFA. Adding to blackhole Group")
             add_user_to_blackhole(username)
-        else :
-            print (username + " has an MFA. Removing from blackhole Group")
+        else:
+            print(username + " has an MFA. Removing from blackhole Group")
             remove_user_from_blackhole(username)
         print("DeactivateMFADevice Execution Complete")
     except ClientError as e:
@@ -80,6 +93,7 @@ def process_DeactivateMFADevice(event, context):
             print("{} no longer exists".format(username))
         else:
             raise
+
 
 def add_user_to_blackhole(username):
     client = boto3.client('iam')
@@ -89,8 +103,9 @@ def add_user_to_blackhole(username):
     )
     if response['ResponseMetadata']['HTTPStatusCode'] != 200:
         handle_error("Adding User to Blackhole Group", username, response['ResponseMetadata'])
-    else: 
+    else:
         return 0
+
 
 def remove_user_from_blackhole(username):
     client = boto3.client('iam')
@@ -100,8 +115,9 @@ def remove_user_from_blackhole(username):
     )
     if response['ResponseMetadata']['HTTPStatusCode'] != 200:
         handle_error("Removing User from Blackhole Group", username, response['ResponseMetadata'])
-    else: 
+    else:
         return 0
+
 
 def handle_error(action, username, ResponseMetadata):
     raise Exception("ERROR" + action + " User: " + username + " Details: " + ResponseMetadata)
